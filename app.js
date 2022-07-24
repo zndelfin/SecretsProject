2070f4eac648e040a7dd332ed5eb5baa37e9aee8
zndelfin <zndelfin.dev@gmail.com>
2022-07-24 03:49:24 -0400

    Salting and Hashing of Passwords

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 //const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+//const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -19,7 +21,9 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 const userSchema = new mongoose.Schema({
   email: String,
@@ -32,47 +36,55 @@ const userSchema = new mongoose.Schema({
 const User = new mongoose.model("User", userSchema);
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
 
-app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
 });
 
 
-app.post("/login", function(req, res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
-  User.findOne({email: username}, function(err, foundUser){
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser){
-        if (foundUser.password === password){
-           res.render("secrets");
-        }
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
   });
@@ -80,6 +92,6 @@ app.post("/login", function(req, res){
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server has started on port 3000");
 })
